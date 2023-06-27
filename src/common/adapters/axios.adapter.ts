080bf9b20462f420fbd61e4553b28d41fa97c549
Franco Cabrera <cabrera.franco@outlook.com>
2023-06-26 23:17:22 -0300

    add poke api

---
+++ src/common/adapters/axios.adapter.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { HttpAdapter } from '../interface/http-adapter.interface';
+import axios, { AxiosInstance } from 'axios';
+
+@Injectable()
+export class AxiosAdapter implements HttpAdapter {
+  private axios: AxiosInstance = axios;
+  async get<T>(url: string): Promise<T> {
+    try {
+      const { data } = await this.axios.get<T>(url);
+      return data;
+    } catch (e) {
+      throw new Error('this is an error - Check logs');
+    }
+  }
+}
